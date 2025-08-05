e1649434e1860badd9d45484ec743e35d14d0e54
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2025-08-05 15:59:26 +0200

    fixed ListView text color and performance issues

--- src/main/java/com/example/mentis/presentation/MainMenuViewController.java
+++ src/main/java/com/example/mentis/presentation/MainMenuViewController.java
@@ -4,6 +4,7 @@ import com.example.mentis.business.data.Examination;
 import com.example.mentis.business.data.Member;
 import com.example.mentis.business.data.Project;
 import com.example.mentis.business.logic.Manager;
+import com.example.mentis.business.logic.Side;
 import com.example.mentis.business.logic.View;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -19,6 +20,9 @@ public class MainMenuViewController implements Controller {
     @FXML
     protected void onSomeProject() {
         Project testProject = new Project("normal", 256, 5, "MyProject");
+        for (int i = 0; i < 20; i++) {
+            testProject.addMember(new Member(12, Side.RIGHT));
+        }
         manager.currentProjectProperty().set(testProject);
         manager.currentViewProperty().set(View.PROJECT);
     }
--- src/main/java/com/example/mentis/presentation/MemberListCell.java
+++ src/main/java/com/example/mentis/presentation/MemberListCell.java
@@ -9,24 +9,30 @@ import java.io.IOException;
 
 public class MemberListCell extends ListCell<Member> {
 
+    private Parent root;
+    private MemberListElementController controller;
+
+    public MemberListCell() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/mentis/Components/MemberListElement.fxml"));
+            root = loader.load();
+            controller = loader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void updateItem(Member member, boolean empty) {
         super.updateItem(member, empty);
 
         if (empty || member == null) {
             setGraphic(null);
+            setText(null);
         } else {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/mentis/Components/MemberListElement.fxml"));
-                Parent root = loader.load();
-                MemberListElementController controller = loader.getController();
-                controller.setMember(member);
-
-                setGraphic(root);
-            } catch (IOException e) {
-                e.printStackTrace();
-                setGraphic(null);
-            }
+            setGraphic(root);
+            setText(null);
+            controller.setMember(member);
         }
     }
 
