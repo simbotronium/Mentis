7aaad053cfdcd7a1092e89da238e9d4ba0ec6a1f
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2025-08-16 20:12:18 +0200

    added animation to voxel

--- src/main/java/com/example/mentis/presentation/Triangle.java
+++ src/main/java/com/example/mentis/presentation/Triangle.java
@@ -1,13 +1,19 @@
 package com.example.mentis.presentation;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
+import javafx.util.Duration;
 
 public class Triangle extends Polygon {
 
     private final double space;
+    private Color fillColor;
 
     public Triangle(StackPane container, Color fillColor, Color strokeColor, double space, SimpleBooleanProperty renderProperty) {
         super();
@@ -21,7 +27,10 @@ public class Triangle extends Polygon {
         super.setStrokeWidth(1.0);
 
         this.space = space;
+        this.fillColor = fillColor;
         renderProperty.addListener((observable, oldValue, newValue) -> updateTriangle(container));
+
+        setupHoverEffect(container);
     }
 
     private void updateTriangle(StackPane container) {
@@ -30,6 +39,24 @@ public class Triangle extends Polygon {
                 container.getWidth() * space, 0.0,
                 container.getWidth() * space / 2.0, container.getHeight() * space
         );
+    }
+
+    private void setupHoverEffect(StackPane container) {
+        container.setOnMouseEntered(e -> animateHover(container, 2.0, fillColor.deriveColor(0, 1, 1, 0.8), 150));
+        container.setOnMouseExited(e -> animateHover(container, 1, fillColor.deriveColor(0, 1, 1, 0.5), 200));
+    }
+
+    private void animateHover(StackPane container, double factor, Color targetFill, int ms) {
+        double dy = (container.getHeight() * space) * (factor - 1);
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.millis(ms),
+                        new KeyValue(scaleXProperty(), factor),
+                        new KeyValue(scaleYProperty(), factor),
+                        new KeyValue(translateYProperty(), -dy),
+                        new KeyValue(fillProperty(), targetFill, Interpolator.EASE_BOTH)
+                )
+        );
+        timeline.play();
     }
 
 }
